902a900e5b4620820961d4f9b90c29f55d030131
Berke Sülüş <berkesulus@trair.com.tr>
2024-07-22 01:41:58 +0300

    database config added

---
+++ pkg/database/mongodb.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoClient *mongo.Client
+
+func ConnectMongoDB(uri string) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	MongoClient = client
+	log.Println("Connected to MongoDB!")
+}
+
+func GetMongoDBCollection(databaseName, collectionName string) *mongo.Collection {
+	return MongoClient.Database(databaseName).Collection(collectionName)
+}
